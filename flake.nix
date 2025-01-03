a87431dd3a8bbdcdbf330c7603b2a9c152a4530e
agustinvalencia <valencia.ag@gmail.com>
2025-01-03 18:00:16 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,106 @@
+{
+  description = "AguLabs macOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+  let
+    configuration = { pkgs, config, ... }: {
+
+      nixpkgs.config.allowUnfree = true;
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [
+          pkgs.kitty
+          pkgs.mkalias
+          pkgs.neovim
+          pkgs.obsidian
+          pkgs.tmux
+	  pkgs.git
+	  pkgs.uv
+	  pkgs.bun
+	  pkgs.vscode
+	  pkgs.aerospace
+	  pkgs.jankyborders
+        ];
+
+      homebrew = {
+        enable = true;
+        brews = [
+          "mas"
+        ];
+        casks = [
+          "hammerspoon"
+          "firefox"
+          "the-unarchiver"
+        ];
+        onActivation.cleanup = "zap";
+      };
+
+      system.defaults = {
+        dock.autohide  = true;
+        dock.largesize = 64;
+        dock.persistent-apps = [
+          "${pkgs.kitty}/Applications/kitty.app"
+          "/Applications/Firefox.app"
+          "/System/Applications/Safari.app"
+          "/System/Applications/Mail.app"
+          "/System/Applications/Calendar.app"
+          "/System/Applications/Spotify.app"
+          "${pkgs.obsidian}/Applications/Obsidian.app"
+        ];
+        finder.FXPreferredViewStyle = "clmv";
+        loginwindow.GuestEnabled  = false;
+        NSGlobalDomain.AppleICUForce24HourTime = true;
+      };
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    darwinConfigurations."mini" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            # Apple Silicon Only
+            enableRosetta = true;
+            # User owning the Homebrew prefix
+            user = "agustinvalencia";
+          };
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."mini".pkgs;
+  };
+}
