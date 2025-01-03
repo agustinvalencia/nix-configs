7eab78bf0bf42422d0a3bd9bb905bea5454e0a11
agustinvalencia <valencia.ag@gmail.com>
2025-01-03 20:26:29 +0100

    raycast to find nix apps

--- flake.nix
+++ flake.nix
@@ -31,7 +31,13 @@
 	  pkgs.jankyborders
 	  pkgs.stow
 	  pkgs.oh-my-posh
+	  pkgs.raycast
         ];
+
+      fonts.packages = with pkgs; [
+        fira-code
+	fira-code-symbols
+      ];
 
       homebrew = {
         enable = true;
@@ -45,6 +51,26 @@
         ];
         onActivation.cleanup = "zap";
       };
+
+     system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
 
       system.defaults = {
         dock.autohide  = true;
